09ed2a9886f0ff1fbab068e6232a05ca6fb072d5
Chris Olstrom <chris@olstrom.com>
2015-10-17 22:46:39 -0700

    Check for linked syslog container, forward logs if possible.

--- syslog.sh
+++ syslog.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-syslogd -n -C &
+if [ -z $SYSLOG_PORT_514_UDP_ADDR ]; then
+  syslogd -n -C &
+else
+  syslogd -n -C -L -R $SYSLOG_PORT_514_UDP_ADDR &
+fi
 
 until netstat -px | grep -s -E '^unix[^/]+/syslog' >/dev/null; do
   sleep .1
